b3e44f3c01be488620fa01cb005023df8fa80822
rete <78441988+retegg@users.noreply.github.com>
2023-09-01 18:29:18 +0200

    Update install.sh

--- ecosmart/install.sh
+++ ecosmart/install.sh
@@ -5,7 +5,7 @@ echo "|  __| / __/ _ \/ __| '_ ` _ \ / _` | '__| __|"
 echo "| |___| (_| (_) \__ \ | | | | | (_| | |  | |_ "
 echo "|______\___\___/|___/_| |_| |_|\__,_|_|   \__|"
                                                
-                                               
+ cd ../                                              
 cp -r ./Ecosmart/* ./
 rm -rf ./Ecosmart
 echo "REINICIA HOME ASSISTANT"
