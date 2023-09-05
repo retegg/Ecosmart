8d47193d743ca06992fdd82ff39fff2e8bde3d79
rete <78441988+retegg@users.noreply.github.com>
2023-09-05 10:27:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
-echo "  ______                                   _" 
+#!/bin/bash
+
+echo "  ______                                   _"
 echo "|  ____|                                 | |"
-echo "| |__   ___ ___  ___ _ __ ___   __ _ _ __| |_ "
+echo "| |__   ___ ___  ___ _ __ ___   __ _ _ __| |_"
 echo "|  __| / __/ _ \/ __| '_ ` _ \ / _` | '__| __|"
-echo "| |___| (_| (_) \__ \ | | | | | (_| | |  | |_ "
+echo "| |___| (_| (_) \__ \ | | | | | (_| | |  | |_"
 echo "|______\___\___/|___/_| |_| |_|\__,_|_|   \__|"
-                                               
- cd ../                                              
+
+# Cambia al directorio padre
+cd ..
+
+# Copia recursivamente los contenidos de la carpeta "Ecosmart" al directorio actual
 cp -r ./Ecosmart/* ./
+
+# Elimina la carpeta "Ecosmart" y su contenido de manera recursiva
 rm -rf ./Ecosmart
+
 echo "REINICIA HOME ASSISTANT"
